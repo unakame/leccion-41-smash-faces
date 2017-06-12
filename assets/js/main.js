d094c33a015a9cbacc1ecf95ca8d52a76bad8dbe
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-12 10:43:04 -0500

    funcionalidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-
-  $(".cities").change(function() {
-    choiceCities = $(this).val();
-
-    if(choiceCities == "choiceLima"){
+/*$(document).ready(function(){
+  $("#cities").change(function() {
+    var selectedOption = $('#cities option:selected');
+    //choiceCities = $(this).val();
+    if(selectedOption == "choiceLima"){
+      alert("hola");
+    }
+    /*if(choiceCities == "choiceLima"){
        var image = $("<img src='assets/peru/AntonellaGutierrez.jpg'>")
       $(.'photoSpace').append("image");
     }
+});
+}*/
+
+$(document).ready(function(){
+  $('#cities').change(function(){
+    var selectedOption = $('#cities option:selected');
+
+    if(selectedOption.val() == "choiceLima"){
+      var image = $("<img src='assets/peru/AntonellaGutierrez.jpg'>");
+     $('.photoSpace').append(image)
+    }else{
+      alert("chau")
+    }
+  });
 });
